c2f46131e98ffbb85bdc29694a9f836e6cbb719f
Matti Paksula <matti.paksula@iki.fi>
2019-01-19 22:19:06 +0200

    travis

--- build/linux.sh
+++ build/linux.sh
@@ -3,5 +3,5 @@ set -e
 
 version=${1#"v"}
 package="tmp/pyttyd-linux-amd64-${version}"
-rubyc -o "$package" -d /tmp/pytty-build --make-args="-j$((`nproc`+1)) --silent" pyttyd
+rubyc -o "$package" -d /tmp/build --make-args="-j$((`nproc`+1)) --silent" pyttyd
 ./"$package" --version
