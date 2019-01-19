e4872adf02eb7969d96425f3728c71099e23b421
Matti Paksula <matti.paksula@iki.fi>
2019-01-19 22:32:39 +0200

    sudo el classico

--- build/macos-deps.sh
+++ build/macos-deps.sh
@@ -7,5 +7,5 @@ brew install openssl || brew upgrade openssl || true
 
 curl -sL https://curl.haxx.se/ca/cacert.pem > /usr/local/etc/openssl/cacert.pem
 
-curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-0.5.0-extra2-darwin-amd64.gz | sudo gunzip > /usr/local/bin/rubyc
+curl -sL https://dl.bintray.com/kontena/ruby-packer/0.5.0-dev/rubyc-0.5.0-extra2-darwin-amd64.gz | gunzip | sudo tee /usr/local/bin/rubyc > /dev/null
 chmod +x /usr/local/bin/rubyc
