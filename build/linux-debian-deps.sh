cfe47b19664beca0747d48b49c120e51f866e829
Matti Paksula <matti.paksula@iki.fi>
2019-01-19 22:22:44 +0200

    sudo

--- build/linux-debian-deps.sh
+++ build/linux-debian-deps.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 set -e
 
-apt-get update && apt-get install -y squashfs-tools build-essential bison curl
+sudo apt-get update && sudo apt-get install -y squashfs-tools build-essential bison curl
 
 curl -L https://github.com/kontena/ruby-packer/releases/download/0.5.0%2Bextra7/rubyc-0.5.0+extra7-linux-amd64.gz | gunzip > /usr/local/bin/rubyc
 chmod +x /usr/local/bin/rubyc
